74bea7c3260d9ecc1196e1714c3f41f8753703ee
twtrubiks <twtrubiks@gmail.com>
2026-03-26 14:38:16 +0800

    上傳影片標題改為選填，未填時自動以檔名作為預設標題

--- videos/forms.py
+++ videos/forms.py
@@ -1,3 +1,5 @@
+import os
+
 from django import forms
 
 from .models import Category, Video
@@ -20,7 +22,17 @@ class VideoUploadForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields["title"].required = False
         # For editing, video_file and thumbnail are not required
         if self.instance and self.instance.pk:
             self.fields["video_file"].required = False
             self.fields["thumbnail"].required = False
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+        video_file = cleaned_data.get("video_file") or self.files.get("video_file")
+        if not title and video_file:
+            filename = getattr(video_file, "name", "")
+            cleaned_data["title"] = os.path.splitext(filename)[0][:255]
+        return cleaned_data
